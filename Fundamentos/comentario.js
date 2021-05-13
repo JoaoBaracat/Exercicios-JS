dff20c836213efab69a8d99237b3ef4269c5eabc
baracat <joaodobingo@gmail.com>
2021-05-13 08:01:51 -0300

    feat: fundamentos JS

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,17 @@
+//Comentario de linha
+console.log('linha 1')
+
+
+/*
+Comentario
+multiplas
+linhas
+*/
+console.log('linha 2')
+
+/*
+* Comentario
+* multplas
+* linhas
+* com asterisco (somente por padrao)
+*/
---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,18 @@
+let qualquer = 'legal'
+
+console.log(qualquer)
+
+console.log(typeof qualquer)
+
+qualquer = 3.1564
+
+console.log(qualquer)
+
+console.log(typeof qualquer)
+
+//evite nomes generigos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto quimico perigoso kkk
+
+
